5aa15c48c2768602f1fa0e94599f998050723071
Atsantiago <atsantiago089@gmail.com>
2018-01-31 01:00:50 -0700

    Assignment 23 completed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function onReady() {
+  let id = 0;
   let toDos = [];
   const addToDOForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
@@ -6,25 +7,28 @@ function onReady() {
 
   function createNewToDo() {
     if (!newToDoText.value) {return; }
-
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id
     });
-
+    id++;
     newToDoText.value = '';
-
     renderTheUI();
+  }
+
+  function deleteToDo(id){
+    toDos = toDos.filter(item => item.id !== id);
   }
 
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+
       checkbox.type = "checkbox";
 
       newLi.textContent = toDo.title;
@@ -34,6 +38,16 @@ function onReady() {
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+
+      const deleteButton = document.createElement("button");
+      newLi.appendChild(deleteButton);
+      deleteButton.innerHTML = "Delete"
+
+     deleteButton.addEventListener("click", event => {
+       deleteToDo(toDo.id);
+       event.preventDefault();
+       renderTheUI();
+     })
     });
   }
 
